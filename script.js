8f989bc3c87eecb9d3371b6c4bb0b8514f5e0412
codingtech <codingtech390@gmail.com>
2024-05-29 00:25:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const images = document.querySelectorAll(".header-slider ul img");
+const prevBtn = document.querySelector(".control-prev");
+const nextBtn = document.querySelector(".control-next");
+
+const scrollContainer = document.querySelectorAll(".products");
+
+let n = 0;
+
+function changeSlider() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  images[n].style.display = "block";
+}
+
+changeSlider();
+
+prevBtn.addEventListener("click", (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = images.length - 1;
+  }
+  changeSlider();
+});
+
+nextBtn.addEventListener("click", (e) => {
+  if (n < images.length - 1) {
+    n++;
+  } else {
+    n = 0;
+  }
+  changeSlider();
+});
+
+for (const item of scrollContainer) {
+  item.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    item.scrollLeft += e.deltaY;
+  });
+}
